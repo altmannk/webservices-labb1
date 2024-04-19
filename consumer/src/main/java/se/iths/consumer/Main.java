3cec497b15f7722a85a042dec185d65c33d5c336
altmannk <katherine.altmann@iths.se>
2024-04-19 12:27:42 +0200

    Add menu with choices

--- consumer/src/main/java/se/iths/consumer/Main.java
+++ consumer/src/main/java/se/iths/consumer/Main.java
@@ -6,17 +6,84 @@ import java.util.Scanner;
 import java.util.ServiceLoader;
 
 public class Main {
-    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("Enter amount in SEK: ");
-        double amountSEK = Double.parseDouble(scanner.nextLine());
+        Scanner scanner = new Scanner(System.in);
+        double amountSEK;
+        int choiceMenu;
 
-        ServiceLoader<CurrencyExchange> loader = ServiceLoader.load(CurrencyExchange.class);
+        while (true) {
+            System.out.print("Enter amount in SEK: ");
+            try {
+                amountSEK = Double.parseDouble(scanner.nextLine());
+                if (amountSEK < 0) {
+                    System.out.println("Invalid input: " + amountSEK + ". Try again.");
+                    continue;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Try again.");
+                continue;
+            }
 
-        for (CurrencyExchange currencyExchange : loader) {
-            System.out.println(currencyExchange.getCurrency(amountSEK));
+            while (true) {
+                printMenu();
+                System.out.print("Choose an option: ");
+                try {
+                    choiceMenu = Integer.parseInt(scanner.nextLine());
+                    if (choiceMenu < 1 || choiceMenu > 5) {
+                        System.out.println("Invalid input: " + choiceMenu + ". Try again.");
+                        continue;
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid input. Try again.");
+                    continue;
+                }
+
+                switch (choiceMenu) {
+                    case 1 -> getDesiredCurrencyExchange("EUR", amountSEK);
+                    case 2 -> getDesiredCurrencyExchange("SKW", amountSEK);
+                    case 3 -> getAllCurrencyExchange(amountSEK);
+                    case 4 -> {}
+                    case 5 -> {
+                        scanner.close();
+                        System.exit(0);
+                    }
+                    default -> throw new IllegalStateException("Unexpected value: " + choiceMenu);
+                }
+
+                if (choiceMenu == 4) break;
+
+            }
         }
 
     }
+
+    private static void printMenu() {
+        System.out.println("""
+                \nMenu:
+                1. EUR
+                2. SKW
+                3. Print all currency
+                4. Enter new value
+                5. Exit
+                """);
+    }
+
+    private static void getDesiredCurrencyExchange(String desiredCurrency, double amountSEK) {
+        ServiceLoader<CurrencyExchange> loader = ServiceLoader.load(CurrencyExchange.class);
+        for (CurrencyExchange currencyExchange : loader) {
+            if (currencyExchange.getClass().getSimpleName().equals(desiredCurrency)) {
+                System.out.println(currencyExchange.getCurrency(amountSEK));
+                break;
+            }
+        }
+    }
+
+    private static void getAllCurrencyExchange(double amountSEK) {
+        ServiceLoader<CurrencyExchange> loader = ServiceLoader.load(CurrencyExchange.class);
+        for (CurrencyExchange currencyExchange : loader) {
+            System.out.println(currencyExchange.getCurrency(amountSEK));
+        }
+    }
+
 }
